f68eeaad1e6a92c11940f16f8c353f17b89e1c56
Rickey H. <59082426+RickeyHo@users.noreply.github.com>
2026-04-26 10:25:48 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,40 +2,111 @@ from http import HTTPStatus
 
 import pandas as pd
 from flask import Flask, request
-from flask_cors import CORS
 import json
 from flask import Response
 
 unFilteredDf = pd.read_csv("exercises_cleaned.csv")
 
 
-fullBodyParts = ['waist', 'upper legs', 'back', 'lower legs', 'chest', 'upper arms', 'cardio','shoulders', 'lower arms', 'neck']
-upperBodyParts = ['back', 'chest', 'shoulders', 'upper arms', 'lower arms', 'neck']
-lowerBodyParts = ['waist', 'upper legs', 'lower legs', 'cardio']
-push = ['chest', 'shoulders', 'upper arms']
-pull = ['back', 'upper arms', 'lower arms', 'neck']
-legs = ['upper legs', 'lower legs', 'waist']
-equipment = ["body weight", "cable", "leverage machine", "assisted", "medicine ball", "stability ball", "band", "barbell", "rope", "dumbbell", "ez barbell", "sled machine", "upper body ergometer", "kettlebell", "olympic barbell", "weighted", "bosu ball", "resistance band", "roller", "skierg machine", "hammer", "smith machine", "wheel roller", "stationary bike", "tire", "trap bar", "elliptical machine", "stepmill machine"]
+fullBodyParts = [
+    'waist',
+    'upper_legs', 'upper legs',
+    'back',
+    'lower_legs', 'lower legs',
+    'chest',
+    'upper_arms', 'upper arms',
+    'cardio',
+    'shoulders',
+    'lower_arms', 'lower arms',
+    'neck'
+]
+
+upperBodyParts = [
+    'back',
+    'chest',
+    'shoulders',
+    'upper_arms', 'upper arms',
+    'lower_arms', 'lower arms',
+    'neck'
+]
+
+lowerBodyParts = [
+    'waist',
+    'upper_legs', 'upper legs',
+    'lower_legs', 'lower legs',
+    'cardio'
+]
+
+push = [
+    'chest',
+    'shoulders',
+    'upper_arms', 'upper arms'
+]
+
+pull = [
+    'back',
+    'upper_arms', 'upper arms',
+    'lower_arms', 'lower arms',
+    'neck'
+]
+
+legs = [
+    'upper_legs', 'upper legs',
+    'lower_legs', 'lower legs',
+    'waist'
+]
+
+equipment = [
+    "body_weight", "body weight",
+    "cable",
+    "leverage_machine", "leverage machine",
+    "assisted",
+    "medicine_ball", "medicine ball",
+    "stability_ball", "stability ball",
+    "band",
+    "barbell",
+    "rope",
+    "dumbbell",
+    "ez_barbell", "ez barbell",
+    "sled_machine", "sled machine",
+    "upper_body_ergometer", "upper body ergometer",
+    "kettlebell",
+    "olympic_barbell", "olympic barbell",
+    "weighted",
+    "bosu_ball", "bosu ball",
+    "resistance_band", "resistance band",
+    "roller",
+    "skierg_machine", "skierg machine",
+    "hammer",
+    "smith_machine", "smith machine",
+    "wheel_roller", "wheel roller",
+    "stationary_bike", "stationary bike",
+    "tire",
+    "trap_bar", "trap bar",
+    "elliptical_machine", "elliptical machine",
+    "stepmill_machine", "stepmill machine"
+]
+
 df = unFilteredDf
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
+
+
 with open("user.json") as file:
     userDict = json.loads(file.read())
 
-@app.route("/api/schedule", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def getPlan():
 
     json = request.json
 
     level = json["difficulty"]
-
     muscleGroups = json["muscleGroups"]
     equipment = json["equipment"]
 
     dfMuscle = unFilteredDf[unFilteredDf["body_part_group"].isin(muscleGroups)]
     dfEquip = dfMuscle[dfMuscle["equipment_group"].isin(equipment)]
 
-    dayCount = len(json["durationMinutes"])
+    days = json["durationMinutes"]
 
     # Filter by level
     if level == 'beginner':
@@ -50,12 +121,27 @@ def getPlan():
     else:
         df = dfEquip
 
+
+    exerciseDays = []
+    i = 0
+    for day in days:
+        if day is not None:
+            exerciseDays.append(i)
+        i += 1
+
+    dayCount = len(exerciseDays)
+
+
     # Create 1D array of day objects
-    exercises = [{"day": i + 1, "exercises": []} for i in range(dayCount)]
+
+    exercises = [{"day": i + 1, "exercises": []} for i in range(7)]
 
     def add_parts_to_day(day_index, parts):
+
         for part in parts:
-            matching_exercises = df[df["body_part_group"] == part].values.tolist()
+
+            print(part)
+            matching_exercises = df[df["body_part_raw"] == part].values.tolist()
 
             if len(matching_exercises) > 3:
                 for i in range(3):
@@ -64,36 +150,36 @@ def getPlan():
                 exercises[day_index]["exercises"].extend(matching_exercises)
 
     if dayCount == 1:
-        add_parts_to_day(0, fullBodyParts)
+        add_parts_to_day(exerciseDays[0], lowerBodyParts)
 
     elif dayCount == 2:
-        add_parts_to_day(0, upperBodyParts)
-        add_parts_to_day(1, lowerBodyParts)
+        add_parts_to_day(exerciseDays[0], upperBodyParts)
+        add_parts_to_day(exerciseDays[0], lowerBodyParts)
 
     elif dayCount == 3:
-        add_parts_to_day(0, push)
-        add_parts_to_day(1, pull)
-        add_parts_to_day(2, legs)
+        add_parts_to_day(exerciseDays[0], push)
+        add_parts_to_day(exerciseDays[1], pull)
+        add_parts_to_day(exerciseDays[2], legs)
 
     elif dayCount == 4:
-        add_parts_to_day(0, upperBodyParts)
-        add_parts_to_day(1, lowerBodyParts)
-        add_parts_to_day(2, upperBodyParts)
-        add_parts_to_day(3, lowerBodyParts)
+        add_parts_to_day(exerciseDays[0], upperBodyParts)
+        add_parts_to_day(exerciseDays[1], lowerBodyParts)
+        add_parts_to_day(exerciseDays[2], upperBodyParts)
+        add_parts_to_day(exerciseDays[3], lowerBodyParts)
 
     elif dayCount == 5:
-        add_parts_to_day(0, push)
-        add_parts_to_day(1, pull)
-        add_parts_to_day(2, legs)
-        add_parts_to_day(3, upperBodyParts)
-        add_parts_to_day(4, lowerBodyParts)
+        add_parts_to_day(exerciseDays[0], push)
+        add_parts_to_day(exerciseDays[1], pull)
+        add_parts_to_day(exerciseDays[2], legs)
+        add_parts_to_day(exerciseDays[3], upperBodyParts)
+        add_parts_to_day(exerciseDays[4], lowerBodyParts)
     elif dayCount == 6:
-        add_parts_to_day(0, push)
-        add_parts_to_day(1, pull)
-        add_parts_to_day(2, legs)
-        add_parts_to_day(3, upperBodyParts)
-        add_parts_to_day(4, lowerBodyParts)
-        add_parts_to_day(5, push)
+        add_parts_to_day(exerciseDays[0], push)
+        add_parts_to_day(exerciseDays[1], pull)
+        add_parts_to_day(exerciseDays[2], legs)
+        add_parts_to_day(exerciseDays[3], upperBodyParts)
+        add_parts_to_day(exerciseDays[4], lowerBodyParts)
+        add_parts_to_day(exerciseDays[5], push)
     elif dayCount == 7:
         add_parts_to_day(0, push)
         add_parts_to_day(1, pull)
@@ -139,10 +225,8 @@ def displayExerciseById(id):
 
     return exercise.to_dict()
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-
-
-
 
 
